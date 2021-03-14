c4a242963762c258349836d7db6b692901bac2d2
rodrigosrosa89 <rodsiqueirabh@hotmail.com>
2021-03-14 12:41:38 -0300

    first commit

---
+++ src/main/java/com/rodao/osrodao/OsrodaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodao.osrodao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrodaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsrodaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodao/osrodao/OsrodaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodao.osrodao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrodaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

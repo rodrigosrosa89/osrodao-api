525d9c6ef5bd26da0be5bf1b72adf265fd9546ca
rodrigosrosa89 <rodsiqueirabh@hotmail.com>
2021-03-18 01:24:11 -0300

    Implementando JPA e Repository

--- src/main/java/com/rodao/osrodao/controller/ClienteController.java
+++ src/main/java/com/rodao/osrodao/controller/ClienteController.java
@@ -1,25 +1,39 @@
 package com.rodao.osrodao.controller;
 
 import java.util.List;
+import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodao.osrodao.domain.model.Cliente;
+import com.rodao.osrodao.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+	@Autowired
+	private ClienteRepository clienteRepository;
 
-	@GetMapping("/clientes")
-	public List<Cliente> listar() {
-		return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
+	@GetMapping
+	public List<Cliente> listarTodos() {
+		return clienteRepository.findAll();
+	}
 
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> getById(@PathVariable("clienteId") Long clienteId) {
+		Optional<Cliente> optional = clienteRepository.findById(clienteId);
+
+		if (optional.isPresent()) {
+			return ResponseEntity.ok(optional.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 }

8a34a0f3649eb88241514b106eeaf89269e5e02a
rodrigosrosa89 <rodsiqueirabh@hotmail.com>
2021-04-03 17:18:13 -0300

    Incluindo comentario

--- src/main/java/com/rodao/osrodao/api/request/OrdemServicoRequest.java
+++ src/main/java/com/rodao/osrodao/api/request/OrdemServicoRequest.java
@@ -6,33 +6,38 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-
 public class OrdemServicoRequest {
 
 	@NotBlank
 	private String descricao;
-	
+
 	@NotNull
 	private BigDecimal preco;
-	
+
 	@Valid
 	@NotNull
 	private ClienteIdRequest clienteId;
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public BigDecimal getPreco() {
 		return preco;
 	}
+
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
+
 	public ClienteIdRequest getClienteId() {
 		return clienteId;
 	}
+
 	public void setClienteId(ClienteIdRequest clienteId) {
 		this.clienteId = clienteId;
 	}
--- src/main/java/com/rodao/osrodao/domain/service/GestaoOrdemService.java
+++ src/main/java/com/rodao/osrodao/domain/service/GestaoOrdemService.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.rodao.osrodao.domain.exception.NegocioException;
 import com.rodao.osrodao.domain.model.Cliente;
+import com.rodao.osrodao.domain.model.Comentario;
 import com.rodao.osrodao.domain.model.OrdemServico;
 import com.rodao.osrodao.domain.model.StatusOrdemServicoEnum;
 import com.rodao.osrodao.domain.repository.ClienteRepository;
+import com.rodao.osrodao.domain.repository.ComentarioRepository;
 import com.rodao.osrodao.domain.repository.OrdemServicoRepository;
 
 @Service
@@ -22,6 +24,9 @@ public class GestaoOrdemService {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	@Autowired
+	private ComentarioRepository comentarioRepository;
+	
 	public OrdemServico criar(@RequestBody OrdemServico ordemServico) {
 		Cliente cliente = clienteRepository.findById(ordemServico.getCliente().getId()).orElseThrow(
 				() -> new NegocioException("Cliente não encontrado"));
@@ -31,6 +36,18 @@ public class GestaoOrdemService {
 		ordemServico.setDataAbertura(OffsetDateTime.now());
 		ordemServicoRepository.save(ordemServico);
 		return ordemServico;
+	}
+	
+	public Comentario adicionarComentario(Long ordemServicoId, String descricao) {
+		OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId)
+				.orElseThrow(() -> new NegocioException("Ordem serviço não encontrada")); 
+		
+		Comentario comentario = new Comentario();
+		comentario.setDataEnvio(OffsetDateTime.now());
+		comentario.setDescricao(descricao);
+		comentario.setOrdemServico(ordemServico);
+			
+		return comentarioRepository.save(comentario);
 	}
 
 }
